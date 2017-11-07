7bdee975359c051158d8ed9923b91770ec1300dd
Daniil Vereschagin <vereshhagin98@mail.ru>
2017-11-07 22:15:02 +0200

    workflow setup

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const SassModuleImporter = require('sass-module-importer');
+const notify = require('gulp-notify');
+const minify = require('gulp-csso');
+const mqpacker = require('gulp-combine-mq');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+
+gulp.task('styles', ()=> {
+    return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass({importer: SassModuleImporter()}))
+    .on('error', notify.onError(function(error) {
+        return {
+            title: 'Styles',
+            messsage: error.messsage
+        }
+    }))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade:false
+    }))
+    .pipe(mqpacker({
+        beautify: true
+    }))
+    .pipe(gulp.dest('./src/dist'))
+    .pipe(minify())
+    .pipe(rename('styles.min.css'))
+    .pipe(gulp.dest('./src/dist/'))
+})
